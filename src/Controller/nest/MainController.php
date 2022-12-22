c94d0202be5dd7a0379e010ee94395c68025ccd0
Ghayth Frikha <ghayth.frikha14@gmail.com>
2022-12-22 23:08:25 +0100

    second commit

--- src/Controller/nest/MainController.php
+++ src/Controller/nest/MainController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class BlameController extends AbstractController
+class MainController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
     public function index(AuthenticationUtils $authenticationUtils): Response
